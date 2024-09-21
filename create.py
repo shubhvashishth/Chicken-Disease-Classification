1cbb53ed99652046f638e0dc90e0c32de66bb48c
Shubham Vashishth <79966896+shubhvashishth@users.noreply.github.com>
2024-09-22 00:19:39 +0530

    add :  Initial structure

---
+++ create.py
@@ -0,0 +1,42 @@
+import os 
+from pathlib import Path
+import logging 
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+project_name = "cnnClassifier"
+
+files = [
+     ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "dvc.yaml",
+    "params.yaml",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb",
+    "templates/index.html"
+]
+
+for filepath in files :
+    filepath = Path(filepath)
+    filedir,filename = os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f"Creating {filedir} for file : {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath,"w"):
+            pass 
+            logging.info(f"Creating {filepath}")
+        
+    else :
+        logging.info(f"{filepath} already exists")
